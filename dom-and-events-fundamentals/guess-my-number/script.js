5d0ac3a945280c095defdeac0a78220973e5187e
Rishi-Ydv <ydvrishi1@gmail.com>
2026-09-25 16:01:21 +0545

    feat(guess-my-number): add visual feedback for correct guess

--- dom-and-events-fundamentals/guess-my-number/script.js
+++ dom-and-events-fundamentals/guess-my-number/script.js
@@ -6,10 +6,19 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width='30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
