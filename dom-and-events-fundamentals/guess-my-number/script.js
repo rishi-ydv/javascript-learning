a056b922d926deecb4014bcdd0070b6c72c7e480
Rishi-Ydv <ydvrishi1@gmail.com>
2026-09-26 12:29:53 +0545

    feat(guess-my-number): display secret number when guess is correct

--- dom-and-events-fundamentals/guess-my-number/script.js
+++ dom-and-events-fundamentals/guess-my-number/script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width='30rem';
