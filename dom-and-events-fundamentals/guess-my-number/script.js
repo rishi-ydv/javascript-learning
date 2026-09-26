6940c10f339d0519390a991da1bcd5527d21a9d3
Rishi-Ydv <ydvrishi1@gmail.com>
2026-09-26 12:17:03 +0545

    feat(guess-my-number): Increase starting score value to 20

--- dom-and-events-fundamentals/guess-my-number/script.js
+++ dom-and-events-fundamentals/guess-my-number/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 3;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
